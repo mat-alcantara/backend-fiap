95363929c9e4764cdb07f1bccfec8ae865c81e52
mat-alcantara <mateuscastro.al@gmail.com>
2023-07-06 06:08:25 -0300

    add hello world

--- src/main/java/com/godigital/inove/InoveApplication.java
+++ src/main/java/com/godigital/inove/InoveApplication.java
@@ -3,13 +3,14 @@ package com.godigital.inove;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = "com.godigital.inove.repository")
 @EntityScan(basePackages = "com.godigital.inove.model")
+@ComponentScan("com.godigital.inove.controller")
 public class InoveApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(InoveApplication.class, args);
 	}
